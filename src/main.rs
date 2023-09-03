e6148ccc019f8a8e86481c5dd178c6fe89e075ca
Caleb Hattingh <caleb.hattingh@gmail.com>
2023-09-03 16:25:01 +0200

    Working solution

--- src/main.rs
+++ src/main.rs
@@ -6,45 +6,90 @@ struct Fork;
 
 struct Philosopher {
     name: String,
+    indent: usize,
     left_fork: Arc<Mutex<Fork>>,
     right_fork: Arc<Mutex<Fork>>,
     thoughts: mpsc::Sender<String>,
 }
 
 impl Philosopher {
+    fn print(&self, s: &str) {
+        let indent = " ".repeat(self.indent);
+        println!("{}{}: {}", indent, &self.name, s);
+    }
+
     fn think(&self) {
-        println!("{} is thinking...", &self.name);
+        // Create an empty string with `indent` spaces
+        self.print("is thinking...");
+        random_sleep(1, 10);
         self.thoughts
             .send(format!("Eureka! {} has a new idea!", &self.name))
             .unwrap();
     }
 
     fn eat(&self) {
-        println!("{} is grabbing forks...", &self.name);
-        self.left_fork.lock().unwrap();
-        self.right_fork.lock().unwrap();
-        println!("{} is eating...", &self.name);
-        thread::sleep(Duration::from_millis(10));
+        let _locks = loop {
+            self.print("trying to get both forks...");
+            let r1 = self.left_fork.try_lock();
+            if r1.is_ok() {
+                self.print("got left fork!");
+            }
+            random_sleep(1, 1000);
+            let r2 = self.right_fork.try_lock();
+            if r2.is_ok() {
+                self.print("got right fork!");
+            }
+            if r1.is_ok() && r2.is_ok() {
+                self.print("got both forks!");
+                break (r1.unwrap(), r2.unwrap());
+            } else {
+                if r1.is_ok() {
+                    self.print("can't get right fork, returning left fork");
+                }
+                if r2.is_ok() {
+                    self.print("can't get left fork, returning right fork");
+                }
+                // self.print("can't get both forks, waiting...");
+                drop(r1);
+                drop(r2);
+            }
+            random_sleep(100, 101);
+        };
+        // self.print("is waiting left fork...");
+        // let _left_fork_guard = self.left_fork.lock().unwrap();
+        // random_sleep(1, 1000);
+        // self.print("is waiting right fork...");
+        // let _right_fork_guard = self.right_fork.lock().unwrap();
+        self.print("is eating...");
+        random_sleep(1, 10);
+        self.print("is returning forks...");
     }
 }
 
-static PHILOSOPHERS: &[&str] = &["Socrates", "Plato", "Aristotle", "Thales", "Pythagoras"];
+static PHILOSOPHERS: &[&str] = &["Socrates", "Plato", "Aristotle", "Thales", "Kant"];
 
 fn main() {
     // Create forks
     // let forks = Arc::new(vec![Mutex::new(Fork), Mutex::new(Fork)]);
-    let forks = (1..PHILOSOPHERS.len() + 2).map(|_| Arc::new(Mutex::new(Fork))).collect::<Vec<_>>();
+    let mut forks = (0..PHILOSOPHERS.len())
+        .map(|_| Arc::new(Mutex::new(Fork)))
+        .collect::<Vec<_>>();
+
+    // Clone the first fork into the last position.
+    forks.push(forks[0].clone());
 
     let (tx, rx) = mpsc::channel();
     // Create philosophers
     let philosophers: Vec<_> = PHILOSOPHERS
         .iter()
         .zip(forks.windows(2))
-        .map(|(name, pair)| {
+        .enumerate()
+        .map(|(i, (name, pair))| {
             let left_fork = pair[0].clone();
             let right_fork = pair[1].clone();
             let philosopher = Philosopher {
                 name: name.to_string(),
+                indent: i * 30,
                 left_fork,
                 right_fork,
                 thoughts: tx.clone(),
@@ -58,23 +103,39 @@ fn main() {
         .collect();
 
     // Make each of them think and eat 100 times
-    for _ in 0..10 {
-        for p in philosophers.iter() {
-            p.think();
-            p.eat();
+    thread::scope(|s| {
+        for p in philosophers {
+            s.spawn(move || {
+                for _ in 0..100 {
+                    // Make each philosopher think
+                    p.think();
+                    // Make each philosopher eat
+                    p.eat();
+                }
+            });
         }
-        // Make each philosopher think
-
-        // Make each philosopher eat
-    }
+    });
     tx.send("Done".to_string()).unwrap();
 
-    // Output their thoughts
-    loop {
-        let thought = rx.recv().unwrap();
-        if thought == "Done" {
-            break;
+    println!("Number of forks: {}", forks.len() - 1);
+
+    if std::env::args().any(|arg| arg == "--thoughts") {
+        // Output their thoughts
+        use std::io::Write;
+        loop {
+            let thought = rx.recv().unwrap();
+            if thought == "Done" {
+                break;
+            }
+            std::io::stdout().write_all(thought.as_bytes()).unwrap();
         }
-        println!("{}", rx.recv().unwrap());
     }
+}
+
+// Utility function to sleep for a random amount of time
+fn random_sleep(a: u64, b: u64) {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+    let sleep_time = rng.gen_range(a..b);
+    thread::sleep(Duration::from_millis(sleep_time));
 }
